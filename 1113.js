a0dd54f844a2400fa94676690b08c526eb03a16d
Izabella-Nascimento <izabellanascimento1606@gmail.com>
2023-12-14 17:36:40 -0300

    Adicionado exercício 1113 de javascript

---
+++ 1113.js
@@ -0,0 +1,30 @@
+var input = require("fs").readFileSync("stdin", "utf8");
+
+var lines = input.split("\n");
+
+/////////////////////// COLAR NO BEECROWD O CODIGO DESTE PONTO EM DIANTE ///////////////////
+
+
+
+let x;
+let y;
+let firstLine = input.split("\n").map(conversion);
+let secondLine = input.split(" ").map(conversion);
+secondLine.shift();
+
+for(indice = 0; indice < firstLine.length; indice++){
+  x = firstLine[indice];
+  y = secondLine[indice];
+
+  if (x == y){
+    break;
+  } else if (x > y){
+    console.log("Decrescente");
+  } else {
+    console.log("Crescente");
+  }
+}
+
+function conversion(value){
+  return parseInt(value, 10);
+}
